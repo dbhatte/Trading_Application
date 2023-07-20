bbef96a5721d971eb21cbe88bcdb84b5a15fa743
Devendra Bhatte <devendra.bhatte@gmail.com>
2023-07-20 23:25:32 +0200

    Added caching logic to cache strategies and simplified logic

--- src/main/java/com/deutschebank/trading/signal/SignalStrategyFactory.java
+++ src/main/java/com/deutschebank/trading/signal/SignalStrategyFactory.java
@@ -4,33 +4,31 @@ import com.deutschebank.trading.algo.AlgoProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class SignalStrategyFactory {
 
+    private final Map<Integer, AlgoStrategy> strategyMap = new HashMap<>();
     @Autowired
     private AlgoProxy algo;
 
-    public AlgoStrategy getSignalStrategy(int signal) {
-        AlgoStrategy algoStrategy;
-        switch (signal) {
-            case 1:
-                algoStrategy = new ConcreteStrategy(List.of(algo::setUp, () -> algo.setAlgoParam(1, 60), algo::performCalc, algo::submitToMarket));
-                break;
-
-            case 2:
-                algoStrategy = new ConcreteStrategy(List.of(algo::reverse, () -> algo.setAlgoParam(1, 80), algo::submitToMarket));
-                break;
-
-            case 3:
-                algoStrategy = new ConcreteStrategy(List.of(() -> algo.setAlgoParam(1, 90), () -> algo.setAlgoParam(2, 15), algo::performCalc, algo::submitToMarket));
-                break;
-
-            default:
-                algoStrategy = new ConcreteStrategy(List.of(algo::cancelTrades));
-                break;
-        }
-        return algoStrategy;
+    @PostConstruct
+    void init() {
+        putSignalStrategy(1, new ConcreteStrategy(List.of(algo::setUp, () -> algo.setAlgoParam(1, 60), algo::performCalc, algo::submitToMarket)));
+        putSignalStrategy(2, new ConcreteStrategy(List.of(algo::reverse, () -> algo.setAlgoParam(1, 80), algo::submitToMarket)));
+        putSignalStrategy(3, new ConcreteStrategy(List.of(() -> algo.setAlgoParam(1, 90), () -> algo.setAlgoParam(2, 15), algo::performCalc, algo::submitToMarket)));
     }
+
+    public AlgoStrategy getSignalStrategy(int signal) {
+        return strategyMap.getOrDefault(signal, new ConcreteStrategy(List.of(algo::cancelTrades)));
+    }
+
+    public void putSignalStrategy(Integer signal, ConcreteStrategy concreteStrategy) {
+        strategyMap.put(signal, concreteStrategy);
+    }
+
 }
